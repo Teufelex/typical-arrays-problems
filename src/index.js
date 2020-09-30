7b067f4c891256ee4701f3a1688a02d80c5a734c
Teufelex <gloomycoke@gmail.com>
2020-09-30 14:55:24 +0300

    add solution

--- src/index.js
+++ src/index.js
@@ -1,12 +1,35 @@
 
 exports.min = function min (array) {
-  return 0;
+    if (array && array.length > 0) {
+        array.sort(function (a, b) {
+            return a-b;
+        })
+        return array[0];
+    } else {
+            return 0;
+    }
 }
 
 exports.max = function max (array) {
-  return 0;
+    if (array && array.length > 0) {
+        array.sort(function (a, b) {
+            return b-a;
+        })
+        return array[0];
+    } else {
+            return 0;
+    }
 }
 
 exports.avg = function avg (array) {
-  return 0;
+    if (array && array.length > 0) {
+    let total = 0;
+    for(let i = 0; i < array.length; i++) {
+        total += array[i];
+    }
+    let avg = total / array.length;
+    return avg;
+    } else {
+        return 0;
+    }
 }
